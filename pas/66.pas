a4d7a0fc7a56024d7de9bab85fce905476f1f1f6
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-01-22 10:20:44 +0700

    Delete pas directory

--- pas/66.pas
+++
@@ -1,9 +0,0 @@
-uses crt;
-const pi=3.14;
-var c,s:real;
-begin
-  write('Nhap canh vuong:');readln(c);
-  s:=sqr(c*sqrt(2)/2)*pi;
-  write('Dien tich hinh tron la ',s:4:2);
-end.
-{Tinh dien tich hinh tron}
