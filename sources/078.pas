90515b24499f82bf4646029b5a064f68414e7ba1
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-10-21 21:38:57 +0700

    Explain how R1C1 to A1 converter works.

--- sources/078.pas
+++ sources/078.pas
@@ -12,7 +12,7 @@ begin
   assign(f,'078.inp');reset(f);
   read(f,tmp);
   delete(tmp,1,1);
-  for i:=1 to length(tmp) do 
+  for i:=1 to length(tmp) do // Lấy số cột từ file input.
     if tmp[i]='C' then
     begin
       val(copy(tmp,1,i-1),row);
@@ -21,6 +21,8 @@ begin
     end;
   close(f);
   repeat
+  // Nếu số cột tạm thời = 27, tăng chỉ số trước đó lên 1. Cứ thế lặp lại cho tới khi nào hết số cột được nhập vào.
+  // Ví dụ từ Z sang AA, reset chữ đầu tiên về 1 (=A) và tăng chữ thứ 2 lên 1 (A > B > C >...)
   begin
     dec(clm);
     inc(chartmp[1]);
@@ -33,7 +35,7 @@ begin
     if chartmp[7]=27 then incnxt(7);
   end
   until clm=0;
-  for i:=8 downto 1 do if chartmp[i]<>0 then write(base26[chartmp[i]]);
+  for i:=8 downto 1 do if chartmp[i]<>0 then write(base26[chartmp[i]]); // Convert sang Base26 và in ngược + chỉ in những giá trị không phải 0.
   write(row);
 end.
-{Chuyen R1C1 sang A1}
+{Chuyển R1C1 sang A1}
